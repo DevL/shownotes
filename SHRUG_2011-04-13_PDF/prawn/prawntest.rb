ade8568b4523cb75ff780455464fddd1dd0907c9
Lennart Fridén <lennart@devl.se>
2011-12-10 00:28:05 +0100

    Minor, old updates
    Signed-off-by: Lennart Fridén <lennart@devl.se>

--- SHRUG_2011-04-13_PDF/prawn/prawntest.rb
+++ SHRUG_2011-04-13_PDF/prawn/prawntest.rb
@@ -5,7 +5,7 @@ require 'rubygems'
 require 'prawn'
 require "prawn/measurement_extensions"
 
-Prawn::Document.generate("prawn.pdf", :page_layout => :portrait, :page_size => 'A4') do
+pdf = Prawn::Document.new(:page_layout => :portrait, :page_size => 'A4') do
 
   # Let's use som colour
   stroke_color "C000C0"   # purple is nice
@@ -13,7 +13,6 @@ Prawn::Document.generate("prawn.pdf", :page_layout => :portrait, :page_size => '
 
   # Some shapes with various border widths
   stroke_rectangle [0, 200], 250, 175
-
   fill_rounded_rectangle [100, 300], 128, 128, 32
 
   line_width 10
@@ -26,6 +25,14 @@ Prawn::Document.generate("prawn.pdf", :page_layout => :portrait, :page_size => '
   font "Courier"
   font_size 24 
   text_box "Lite text som positionerats med hjälp av 'mänskliga' mått", :at => [5.cm, 128.mm], :height => 1.dm, :width => 85.mm
+
+  # Page numbering
+  font_size 10
+  number_pages "Sida <page> av <total>", :at => [bounds.right - 150, 0], :width => 150, :align => :right, :start_count_at => 1, :color => '808080', :page_filter => :all
 end
+
+
+
+pdf.render_file 'prawn.pdf'
 
 system 'open prawn.pdf'
